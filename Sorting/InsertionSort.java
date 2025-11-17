fbba2c457624df0e76cb723a691c23f0de707567
subratpandeyy <12subratpandey@gmail.com>
2025-11-17 23:40:44 +0530

    Merge Sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -2,7 +2,6 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = {5, 3, 4, 2, 1};
         for(int i = 0; i < arr.length; i++) {
-            int key = arr[i];
             int j = i;
             while(j > 0 && arr[j-1] > arr[j]) {
                 int temp = arr[j-1];
---
+++ Sorting/MergeSort.java
@@ -0,0 +1,60 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 6, 3, 2, 1};
+        MergeSortArray ms = new MergeSortArray();
+        ms.mergesort(arr, 0, arr.length-1);
+        ms.display(arr);
+    }
+}
+class MergeSortArray {
+    public void mergesort(int arr[], int si, int ei) {
+        if(si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si)/2;
+        mergesort(arr, si, mid);
+        mergesort(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+    }
+
+    void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;      // iterator for temp array
+
+        while(i <= mid && j <= ei) {
+            if(arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            }
+            else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        
+        // Left part
+        while(i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right part
+        while(j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copying temp to arr
+        for(k=0, i=si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    // Display method
+    void display(int arr[]) {
+        System.out.print("Sorted Array: ");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
