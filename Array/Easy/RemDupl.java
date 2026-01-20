dc0a64a71fd74cd3df3879ba6a5932d6c9d8ec0a
subratpandeyy <12subratpandey@gmail.com>
2026-01-20 23:00:18 +0530

    remove duplicate

--- Array/Easy/RemDupl.java
+++ Array/Easy/RemDupl.java
@@ -1,14 +1,30 @@
 public class RemDupl {
     public static void main(String args[]) {
-        int arr[] = {1, 2, 3, 4, 5, 5};
+        int arr[] = {1, 2, 3, 4, 4, 5, 5};
         int n = arr.length;
+        Remove obj = new Remove();
+        obj.removeduplicates(arr);
         for(int i = 0; i < n-1; i++) {
-            arr[i] = arr[i];
-            if(arr[i] == arr[i+1]) {
-                arr[i] = arr[i+1];
-            }
             System.out.println(arr[i]);
+            }
+    }
+}
+
+class Remove {
+    public int removeduplicates(int arr[]) {
+        int n = arr.length;
+        if(n == 0) return 0;
+
+        // pointer for last unique element
+        int i = 0;
+
+        for(int j = 1;j < n;j++) {
+            if(arr[j] != arr[i]) {
+                i++;
+            }
+            arr[i] = arr[j];
         }
+        return i+1;
     }
 }
 // NEEDS OPTIMIZATION
