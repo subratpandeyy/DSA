0151c97043eb27076ee412c19e23a515e2d54c18
subratpandeyy <12subratpandey@gmail.com>
2026-01-23 23:21:31 +0530

    RemDupl

--- Array/Easy/RemDupl.java
+++ Array/Easy/RemDupl.java
@@ -3,7 +3,7 @@ public class RemDupl {
         int arr[] = {1, 2, 3, 4, 4, 5, 5};
         int n = arr.length;
         Remove obj = new Remove();
-        obj.removeduplicates(arr);
+        int output = obj.removeduplicates(arr);
         for(int i = 0; i < n-1; i++) {
             System.out.println(arr[i]);
             }
